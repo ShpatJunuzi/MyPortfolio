921bfc80a0f16b9ffcaf51e9912aa84c5b2526e2
Shpat <shpatsentertainment@gmail.com>
2026-07-16 14:14:57 +0200

    first commit

---
+++ shpats-portfolio/src/App.jsx
@@ -0,0 +1,22 @@
+import { Hero } from "@/sections/Hero";
+import { Experience } from "./sections/Experience";
+import { Testimonials } from "./sections/Testimonials";
+import { Contact } from "./sections/Contact";
+import { About } from "./sections/About";
+import { Projects } from "./sections/Projects";
+
+function App() {
+  return <div className="min-h-screen overflow-x-hidden">
+    <Navbar/>
+    <main>
+      <Hero />
+      <About />
+      <Projects />
+      <Experience />
+      <Testimonials />
+      <Contact/>
+    </main>
+  </div>;
+}
+
+export default App;
---
+++ shpats-portfolio/src/layout/Navbar.jsx
@@ -0,0 +1,29 @@
+const navLinks = [
+  { href: "#about", label: "About"}, 
+  { href: "#projects", label: "Projects"}, 
+  { href: "#experience", label: "Experience"}, 
+  { href: "#testimonials", label: "Testimonials"}, 
+];
+
+
+export const Navbar = () => {
+return (
+<header className="dixed top-0 left-0 right-0 bg-transparent py-5">
+  <nav className="container mx-auto px-6 flex items-center justify-between">
+    <a href="#" className="text-xl font-bold tracking-light">
+      PM <span>.</span>
+    </a>
+
+    <div>
+      <div>
+        {navLinks.map((link, index) => (
+          <a href={link.href} key={index}>
+          {link.label}
+          </a>
+        ))}
+      </div>
+    </div>
+  </nav>
+</header>
+);
+};
